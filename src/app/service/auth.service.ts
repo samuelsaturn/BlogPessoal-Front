e5bde59be9876591b17a3c5a0998ca7c9ceb928b
Saturno <samuelmathias033@gmail.com>
2022-04-05 15:40:37 -0300

    @samuelsaturn [Task: POST and GET Postagens]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -14,12 +14,26 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  refreshToken(){
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
+  }
+
   entrar(UsuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{
     return this.http.post<UsuarioLogin>('https://blogsaturn.herokuapp.com/usuarios/logar', UsuarioLogin)
   }
 
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>('https://blogsaturn.herokuapp.com/usuarios/cadastrar', usuario)
+  }
+
+  getByIdUsuario(id:number): Observable<Usuario>{
+    return this.http.get<Usuario>(`https://blogsaturn.herokuapp.com/usuarios/${id}`, this.token)
   }
 
   logado(){
